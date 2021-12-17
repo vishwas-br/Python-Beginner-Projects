f95b431ad46ccc2d5aed41251102ab9765b53bd1
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2021-12-17 12:48:50 +0530

    Update dice_rolling.py

--- Dice Rolling Simulator/dice_rolling.py
+++ Dice Rolling Simulator/dice_rolling.py
@@ -2,13 +2,13 @@ import random
 print("Hey there!")
 ask = str(input("Do you want to roll a dice? (y/n) : "))
 x = random.randint(1,6)
-yep = random.randint(1,5)
+second = random.randint(1,5)
 if ask == "y":
     print("Great! Rolling the dice...")
     print("The number is ..." + str(x))
     if x == 6:
         print("Congrats! Since the number 6 showed up, you get another chance")
         print("Rolling again...")
-        print("The number is ..." + str(yep))
+        print("The number is ..." + str(second))
 else:
     print("No problem. But please do give it a try. ")
